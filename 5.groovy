f219ffc7521f2875ee527fcadeab32ab0052b6dd
Advaid Gireesan <73230570+advaidg@users.noreply.github.com>
2023-09-11 14:29:21 +0530

    Create 5.groovy

---
+++ 5.groovy
@@ -0,0 +1,40 @@
+pipeline {
+    agent any
+
+    environment {
+        SONARQUBE_URL = 'http://your-sonarqube-server-url'
+        API_TOKEN = 'your-api-token'
+        PORTFOLIO_KEY = 'your-portfolio-key'
+        QUALITY_GATES = ['A', 'B', 'C']
+    }
+
+    stages {
+        stage('Fetch Quality Gate Counts') {
+            steps {
+                script {
+                    def projectCountMap = [:]
+
+                    for (qualityGate in QUALITY_GATES) {
+                        def apiUrl = "${SONARQUBE_URL}/api/projects/search?gate=${qualityGate}&portfolio=${PORTFOLIO_KEY}"
+                        def curlCommand = """curl -s -H 'Authorization: Bearer ${API_TOKEN}' ${apiUrl}"""
+                        def response = sh(script: curlCommand, returnStatus: true)
+
+                        if (response == 0) {
+                            def responseData = sh(script: curlCommand, returnStdout: true).trim()
+                            def projectList = responseData.readJSON().components
+                            def projectCount = projectList.size()
+                            projectCountMap[qualityGate] = projectCount
+                        } else {
+                            error "Failed to fetch data for Quality Gate ${qualityGate} from SonarQube."
+                        }
+                    }
+
+                    // Print the project counts for each quality gate
+                    for (qualityGate in QUALITY_GATES) {
+                        echo "Quality Gate ${qualityGate}: ${projectCountMap[qualityGate]} projects"
+                    }
+                }
+            }
+        }
+    }
+}
