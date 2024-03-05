cab87a64a11b520c3447c92d0f6fa2376971ef0c
Advaid Gireesan <73230570+advaidg@users.noreply.github.com>
2024-03-05 19:22:37 +0530

    Update cccmmp.py

--- cccmmp.py
+++ cccmmp.py
@@ -1,18 +1,22 @@
 import xml.etree.ElementTree as ET
 
-def sort_xml_recursive(element, order='ascending'):
-    element[:] = sorted(element, key=lambda child: child.tag, reverse=(order == 'descending'))
-    for child in element:
-        sort_xml_recursive(child, order)
-
-def sort_xml_elements(xml_file, order='ascending'):
+def sort_xml_elements(xml_file, entity_to_sort, order='ascending'):
     tree = ET.parse(xml_file)
     root = tree.getroot()
 
-    sort_xml_recursive(root, order)
+    # Find the entity to sort
+    entity = root.find(".//{}".format(entity_to_sort))
+
+    if entity is not None:
+        # Sort the child elements of the specified entity
+        entity[:] = sorted(entity, key=lambda child: child.tag, reverse=(order == 'descending'))
+    else:
+        print("Entity '{}' not found in the XML file.".format(entity_to_sort))
+        return
 
     # Save the modified XML to a new file or overwrite the original
     tree.write('sorted_' + xml_file)
 
 # Replace 'your_file.xml' with the actual name of your XML file
-sort_xml_elements('your_file.xml', order='ascending')
+# Replace 'EntityToSort' with the tag of the entity you want to sort
+sort_xml_elements('your_file.xml', entity_to_sort='EntityToSort', order='ascending')
