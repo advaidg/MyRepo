bdafb534e01239e4ff9344f840e0764363b0c26e
Advaid Gireesan <73230570+advaidg@users.noreply.github.com>
2024-03-05 19:24:30 +0530

    Update cccmmp.py

--- cccmmp.py
+++ cccmmp.py
@@ -1,22 +1,22 @@
 import xml.etree.ElementTree as ET
 
-def sort_xml_elements(xml_file, entity_to_sort, order='ascending'):
+def sort_xml_elements(xml_file, entity_tag, order='ascending'):
     tree = ET.parse(xml_file)
     root = tree.getroot()
 
     # Find the entity to sort
-    entity = root.find(".//{}".format(entity_to_sort))
+    entities = root.findall(".//{}".format(entity_tag))
 
-    if entity is not None:
-        # Sort the child elements of the specified entity
-        entity[:] = sorted(entity, key=lambda child: child.tag, reverse=(order == 'descending'))
+    if entities:
+        # Sort the entities based on the account number attribute
+        entities.sort(key=lambda entity: entity.get('accountnumber'), reverse=(order == 'descending'))
     else:
-        print("Entity '{}' not found in the XML file.".format(entity_to_sort))
+        print("No entities with tag '{}' found in the XML file.".format(entity_tag))
         return
 
     # Save the modified XML to a new file or overwrite the original
     tree.write('sorted_' + xml_file)
 
 # Replace 'your_file.xml' with the actual name of your XML file
-# Replace 'EntityToSort' with the tag of the entity you want to sort
-sort_xml_elements('your_file.xml', entity_to_sort='EntityToSort', order='ascending')
+# Replace 'AccountEntity' with the tag of the entity you want to sort
+sort_xml_elements('your_file.xml', entity_tag='AccountEntity', order='ascending')
