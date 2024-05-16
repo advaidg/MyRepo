26e6ee2b0b82ae812acf5d11425ce6131e069dc5
Advaid Gireesan <73230570+advaidg@users.noreply.github.com>
2024-05-16 23:27:53 +0530

    Update querycheck_date.sql

--- querycheck_date.sql
+++ querycheck_date.sql
@@ -1,11 +1,10 @@
-WITH DateGenerator AS (
-  SELECT TO_DATE('2022-01-01', 'YYYY-MM-DD') + LEVEL - 1 AS GeneratedDate
+WITH DateRange AS (
+  SELECT TRUNC(TO_DATE('2022-01-01', 'YYYY-MM-DD')) + LEVEL - 1 AS DateGenerated
   FROM dual
-  CONNECT BY LEVEL <= SYSDATE - TO_DATE('2022-01-01', 'YYYY-MM-DD') + 1
+  CONNECT BY TRUNC(TO_DATE('2022-01-01', 'YYYY-MM-DD')) + LEVEL - 1 <= TRUNC(SYSDATE)
 )
-SELECT d.GeneratedDate
-FROM DateGenerator d
-LEFT JOIN your_table_name t
-ON d.GeneratedDate = t.load_date
+SELECT d.DateGenerated
+FROM DateRange d
+LEFT JOIN your_table_name t ON d.DateGenerated = t.load_date
 WHERE t.load_date IS NULL
-ORDER BY d.GeneratedDate;
+ORDER BY d.DateGenerated;
