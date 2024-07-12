0d25af0df20e7c109382973ff52e8734505a52f9
Advaid Gireesan <73230570+advaidg@users.noreply.github.com>
2024-07-12 22:32:22 +0530

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -5,8 +5,8 @@ openssl genpkey -algorithm RSA -out private_key.pem -pkeyopt rsa_keygen_bits:204
 openssl rsa -pubout -in private_key.pem -out public_key.pem
 
 # Remove the PEM headers/footers and encode the keys in Base64
-base64EncodedPrivateKey=$(openssl pkcs8 -topk8 -nocrypt -in private_key.pem | openssl base64 -A)
-base64EncodedPublicKey=$(openssl rsa -in private_key.pem -pubout | openssl base64 -A)
+base64EncodedPrivateKey=$(openssl pkcs8 -topk8 -nocrypt -in private_key.pem | openssl base64 -A | tr -d '\n')
+base64EncodedPublicKey=$(openssl rsa -in private_key.pem -pubout | openssl base64 -A | tr -d '\n')
 
 # Print the Base64 encoded keys
 echo "Base64 Encoded Public Key:"
