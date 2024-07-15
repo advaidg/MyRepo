301a3634d242f160d95aa6d0444725df6d12a311
Advaid Gireesan <73230570+advaidg@users.noreply.github.com>
2024-07-15 14:02:39 +0530

    Update JsonComparator.java

--- JsonComparator.java
+++ JsonComparator.java
@@ -6,10 +6,10 @@ import com.flipkart.zjsonpatch.JsonDiff;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class JsonComparator {
 
@@ -31,27 +31,42 @@ public class JsonComparator {
         }
     }
 
-    public static JsonNode sortJson(JsonNode node) {
-        if (node.isObject()) {
-            ObjectNode sortedNode = new ObjectMapper().createObjectNode();
-            node.fieldNames().asIterator().forEachRemaining(field -> sortedNode.set(field, sortJson(node.get(field))));
-            List<String> sortedKeys = new ArrayList<>();
-            node.fieldNames().forEachRemaining(sortedKeys::add);
-            Collections.sort(sortedKeys);
-            ObjectNode sortedObjNode = new ObjectMapper().createObjectNode();
-            for (String key : sortedKeys) {
-                sortedObjNode.set(key, sortedNode.get(key));
+    public static boolean areJsonNodesEqual(JsonNode node1, JsonNode node2) {
+        if (node1 == null || node2 == null) {
+            return node1 == node2;
+        }
+
+        if (node1.isObject() && node2.isObject()) {
+            Set<String> fieldNames = new HashSet<>();
+            node1.fieldNames().forEachRemaining(fieldNames::add);
+            node2.fieldNames().forEachRemaining(fieldNames::add);
+
+            for (String fieldName : fieldNames) {
+                if (!areJsonNodesEqual(node1.get(fieldName), node2.get(fieldName))) {
+                    return false;
+                }
             }
-            return sortedObjNode;
-        } else if (node.isArray()) {
-            ArrayNode sortedArray = new ObjectMapper().createArrayNode();
-            List<JsonNode> nodeList = new ArrayList<>();
-            node.forEach(nodeList::add);
-            nodeList.sort((a, b) -> a.toString().compareTo(b.toString()));
-            nodeList.forEach(sortedArray::add);
-            return sortedArray;
+            return true;
+        } else if (node1.isArray() && node2.isArray()) {
+            if (node1.size() != node2.size()) {
+                return false;
+            }
+
+            for (int i = 0; i < node1.size(); i++) {
+                boolean matchFound = false;
+                for (int j = 0; j < node2.size(); j++) {
+                    if (areJsonNodesEqual(node1.get(i), node2.get(j))) {
+                        matchFound = true;
+                        break;
+                    }
+                }
+                if (!matchFound) {
+                    return false;
+                }
+            }
+            return true;
         } else {
-            return node;
+            return node1.equals(node2);
         }
     }
 
@@ -69,34 +84,31 @@ public class JsonComparator {
         removeElements(json1, keysToRemove);
         removeElements(json2, keysToRemove);
 
-        // Sort JSON data
-        JsonNode sortedJson1 = sortJson(json1);
-        JsonNode sortedJson2 = sortJson(json2);
-
-        // Save modified and sorted JSON files for inspection (optional)
-        saveJson("sorted_" + file1, sortedJson1);
-        saveJson("sorted_" + file2, sortedJson2);
+        // Save modified JSON files for inspection (optional)
+        saveJson("modified_" + file1, json1);
+        saveJson("modified_" + file2, json2);
 
         // Compare JSON data
-        JsonNode diff = JsonDiff.asJson(sortedJson1, sortedJson2);
-
-        // Print readable summary of differences
-        if (diff.size() > 0) {
+        if (areJsonNodesEqual(json1, json2)) {
+            System.out.println("No differences found. The JSON files are equivalent after removing specified elements.");
+        } else {
             System.out.println("Differences found:");
+            JsonNode diff = JsonDiff.asJson(json1, json2);
             for (JsonNode d : diff) {
                 String op = d.get("op").asText();
                 String path = d.get("path").asText();
+                JsonNode value = d.get("value");
                 switch (op) {
                     case "add":
-                        System.out.println("Added value: " + d.get("value") + " at " + path);
+                        System.out.println("Added value: " + value + " at " + path);
                         break;
                     case "remove":
-                        JsonNode removedValue = sortedJson1.at(path);
+                        JsonNode removedValue = json1.at(path);
                         System.out.println("Removed value: " + removedValue + " from " + path);
                         break;
                     case "replace":
-                        JsonNode oldValue = sortedJson1.at(path);
-                        JsonNode newValue = d.get("value");
+                        JsonNode oldValue = json1.at(path);
+                        JsonNode newValue = value;
                         System.out.println("Replaced value: " + oldValue + " with " + newValue + " at " + path);
                         break;
                     default:
@@ -104,8 +116,6 @@ public class JsonComparator {
                         break;
                 }
             }
-        } else {
-            System.out.println("No differences found. The JSON files are equivalent after removing specified elements and sorting.");
         }
     }
 
