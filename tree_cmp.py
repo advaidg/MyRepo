10c80dd8afa72f5c6c32b829b999815ad7222dd2
Advaid Gireesan <73230570+advaidg@users.noreply.github.com>
2024-07-22 22:18:30 +0530

    Create tree_cmp.py

---
+++ tree_cmp.py
@@ -0,0 +1,55 @@
+import xml.etree.ElementTree as ET
+
+def get_structure(element, path=""):
+    structure = []
+    children = list(element)
+    if not children:
+        return [path + "/" + element.tag]
+    else:
+        structure.append(path + "/" + element.tag)
+        for child in children:
+            structure.extend(get_structure(child, path + "/" + element.tag))
+    return structure
+
+def compare_structures(struct1, struct2):
+    set1 = set(struct1)
+    set2 = set(struct2)
+
+    only_in_first = set1 - set2
+    only_in_second = set2 - set1
+
+    return only_in_first, only_in_second
+
+def print_differences(only_in_first, only_in_second):
+    if only_in_first:
+        print("Elements only in the first XML:")
+        for item in sorted(only_in_first):
+            print(item)
+    else:
+        print("No unique elements in the first XML.")
+
+    if only_in_second:
+        print("Elements only in the second XML:")
+        for item in sorted(only_in_second):
+            print(item)
+    else:
+        print("No unique elements in the second XML.")
+
+def main(file1, file2):
+    tree1 = ET.parse(file1)
+    root1 = tree1.getroot()
+    structure1 = get_structure(root1)
+
+    tree2 = ET.parse(file2)
+    root2 = tree2.getroot()
+    structure2 = get_structure(root2)
+
+    only_in_first, only_in_second = compare_structures(structure1, structure2)
+
+    print_differences(only_in_first, only_in_second)
+
+if __name__ == "__main__":
+    file1 = "file1.xml"  # Replace with your XML file path
+    file2 = "file2.xml"  # Replace with your XML file path
+
+    main(file1, file2)
