8c60d07ed0876a30e7987e61e48b3cf8a98fd0a1
Advaid Gireesan <73230570+advaidg@users.noreply.github.com>
2024-08-07 15:01:12 +0530

    Update xml.py

--- xml.py
+++ xml.py
@@ -2,9 +2,11 @@ import os
 from lxml import etree
 
 class WebXmlParser:
-    def __init__(self, input_file, output_dir):
+    def __init__(self, input_file, output_dir, config_class_name="WebConfig"):
         self.input_file = input_file
         self.output_dir = output_dir
+        self.config_class_name = config_class_name
+        self.config_content = ""
 
     def parse_web_xml(self):
         tree = etree.parse(self.input_file)
@@ -19,46 +21,48 @@ class WebXmlParser:
         self.parse_listeners(root, ns)
         self.parse_error_pages(root, ns)
 
+        self.generate_combined_config_class()
+
     def parse_context_params(self, root, ns):
         context_params = root.findall("ns:context-param", ns)
         for param in context_params:
             param_name = param.find("ns:param-name", ns).text
             param_value = param.find("ns:param-value", ns).text
-            self.generate_context_param_config(param_name, param_value)
+            self.add_context_param_config(param_name, param_value)
 
     def parse_filters(self, root, ns):
         filters = root.findall("ns:filter", ns)
         for filter_element in filters:
             filter_name = filter_element.find("ns:filter-name", ns).text
             filter_class = filter_element.find("ns:filter-class", ns).text
-            self.generate_filter_config(filter_name, filter_class)
+            self.add_filter_config(filter_name, filter_class)
 
     def parse_filter_mappings(self, root, ns):
         filter_mappings = root.findall("ns:filter-mapping", ns)
         for mapping in filter_mappings:
             filter_name = mapping.find("ns:filter-name", ns).text
             url_pattern = mapping.find("ns:url-pattern", ns).text
-            self.generate_filter_mapping_config(filter_name, url_pattern)
+            self.add_filter_mapping_config(filter_name, url_pattern)
 
     def parse_servlets(self, root, ns):
         servlets = root.findall("ns:servlet", ns)
         for servlet in servlets:
             servlet_name = servlet.find("ns:servlet-name", ns).text
             servlet_class = servlet.find("ns:servlet-class", ns).text
-            self.generate_servlet_config(servlet_name, servlet_class)
+            self.add_servlet_config(servlet_name, servlet_class)
 
     def parse_servlet_mappings(self, root, ns):
         servlet_mappings = root.findall("ns:servlet-mapping", ns)
         for mapping in servlet_mappings:
             servlet_name = mapping.find("ns:servlet-name", ns).text
             url_pattern = mapping.find("ns:url-pattern", ns).text
-            self.generate_servlet_mapping_config(servlet_name, url_pattern)
+            self.add_servlet_mapping_config(servlet_name, url_pattern)
 
     def parse_listeners(self, root, ns):
         listeners = root.findall("ns:listener", ns)
         for listener in listeners:
             listener_class = listener.find("ns:listener-class", ns).text
-            self.generate_listener_config(listener_class)
+            self.add_listener_config(listener_class)
 
     def parse_error_pages(self, root, ns):
         error_pages = root.findall("ns:error-page", ns)
@@ -69,144 +73,93 @@ class WebXmlParser:
 
             if error_code is not None:
                 error_code = error_code.text
-                self.generate_error_page_config(error_code, location, is_exception=False)
+                self.add_error_page_config(error_code, location, is_exception=False)
             elif exception_type is not None:
                 exception_type = exception_type.text
-                self.generate_error_page_config(exception_type, location, is_exception=True)
+                self.add_error_page_config(exception_type, location, is_exception=True)
 
-    def generate_context_param_config(self, param_name, param_value):
-        class_name = f"{param_name.capitalize()}Config"
-        content = f"""import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class {class_name} {{
-
-    @Bean
+    def add_context_param_config(self, param_name, param_value):
+        bean_definition = f"""    @Bean
     public String {param_name}() {{
         return "{param_value}";
-    }}
-}}
-"""
-        self.write_to_file(class_name, content)
+    }}\n\n"""
+        self.config_content += bean_definition
 
-    def generate_filter_config(self, filter_name, filter_class):
-        class_name = f"{filter_name.capitalize()}FilterConfig"
-        content = f"""import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import {filter_class};
-
-@Configuration
-public class {class_name} {{
-
-    @Bean
+    def add_filter_config(self, filter_name, filter_class):
+        bean_definition = f"""    @Bean
     public FilterRegistrationBean<{filter_class.split('.')[-1]}> {filter_name}() {{
         FilterRegistrationBean<{filter_class.split('.')[-1]}> registrationBean = 
             new FilterRegistrationBean<>(new {filter_class.split('.')[-1]}());
         registrationBean.addUrlPatterns("/*");
         return registrationBean;
-    }}
-}}
-"""
-        self.write_to_file(class_name, content)
+    }}\n\n"""
+        self.config_content += bean_definition
 
-    def generate_filter_mapping_config(self, filter_name, url_pattern):
-        class_name = f"{filter_name.capitalize()}FilterMappingConfig"
-        content = f"""import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class {class_name} {{
-
-    @Bean
+    def add_filter_mapping_config(self, filter_name, url_pattern):
+        bean_definition = f"""    @Bean
     public FilterRegistrationBean<?> {filter_name}Mapping() {{
         FilterRegistrationBean<?> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(new {filter_name}());
         registrationBean.addUrlPatterns("{url_pattern}");
         return registrationBean;
-    }}
-}}
-"""
-        self.write_to_file(class_name, content)
+    }}\n\n"""
+        self.config_content += bean_definition
 
-    def generate_servlet_config(self, servlet_name, servlet_class):
-        class_name = f"{servlet_name.capitalize()}ServletConfig"
-        content = f"""import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import {servlet_class};
-
-@Configuration
-public class {class_name} {{
-
-    @Bean
+    def add_servlet_config(self, servlet_name, servlet_class):
+        bean_definition = f"""    @Bean
     public ServletRegistrationBean<{servlet_class.split('.')[-1]}> {servlet_name}() {{
         ServletRegistrationBean<{servlet_class.split('.')[-1]}> registrationBean = 
             new ServletRegistrationBean<>(new {servlet_class.split('.')[-1]}(), "/*");
         return registrationBean;
-    }}
-}}
-"""
-        self.write_to_file(class_name, content)
+    }}\n\n"""
+        self.config_content += bean_definition
 
-    def generate_servlet_mapping_config(self, servlet_name, url_pattern):
-        class_name = f"{servlet_name.capitalize()}ServletMappingConfig"
-        content = f"""import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class {class_name} {{
-
-    @Bean
+    def add_servlet_mapping_config(self, servlet_name, url_pattern):
+        bean_definition = f"""    @Bean
     public ServletRegistrationBean<?> {servlet_name}Mapping() {{
         ServletRegistrationBean<?> registrationBean = new ServletRegistrationBean<>();
         registrationBean.setServlet(new {servlet_name}());
         registrationBean.addUrlPatterns("{url_pattern}");
         return registrationBean;
-    }}
-}}
-"""
-        self.write_to_file(class_name, content)
+    }}\n\n"""
+        self.config_content += bean_definition
 
-    def generate_listener_config(self, listener_class):
-        class_name = listener_class.split('.')[-1] + "Config"
-        content = f"""import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import {listener_class};
-
-@Configuration
-public class {class_name} {{
-
-    @Bean
+    def add_listener_config(self, listener_class):
+        bean_definition = f"""    @Bean
     public ServletListenerRegistrationBean<{listener_class.split('.')[-1]}> {listener_class.split('.')[-1]}Listener() {{
         return new ServletListenerRegistrationBean<>(new {listener_class.split('.')[-1]}());
-    }}
-}}
-"""
-        self.write_to_file(class_name, content)
+    }}\n\n"""
+        self.config_content += bean_definition
 
-    def generate_error_page_config(self, error_or_exception, location, is_exception):
+    def add_error_page_config(self, error_or_exception, location, is_exception):
         error_type = "Exception" if is_exception else "Error"
-        class_name = f"{error_or_exception.capitalize()}{error_type}Config"
-        content = f"""import org.springframework.boot.web.servlet.error.ErrorPageRegistrar;
+        bean_definition = f"""    @Bean
+    public ErrorPageRegistrar errorPageRegistrar() {{
+        return new ErrorPageRegistrar() {{
+            @Override
+            public void registerErrorPages(ErrorPageRegistry registry) {{
+                registry.addErrorPages(new ErrorPage({error_or_exception}.class, "{location}"));
+            }}
+        }};
+    }}\n\n"""
+        self.config_content += bean_definition
+
+    def generate_combined_config_class(self):
+        content = f"""import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.error.ErrorPageRegistrar;
 import org.springframework.boot.web.servlet.error.ErrorPageRegistry;
 import org.springframework.boot.web.servlet.error.ErrorPage;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class {class_name} implements ErrorPageRegistrar {{
+public class {self.config_class_name} {{
 
-    @Override
-    public void registerErrorPages(ErrorPageRegistry registry) {{
-        registry.addErrorPages(new ErrorPage({error_or_exception}.class, "{location}"));
-    }}
-}}
+{self.config_content}}}
 """
-        self.write_to_file(class_name, content)
+        self.write_to_file(self.config_class_name, content)
 
     def write_to_file(self, class_name, content):
         if not os.path.exists(self.output_dir):
